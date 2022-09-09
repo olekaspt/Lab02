5933420aa1c9cd0fa2bc845a2bc49e2815ae68e3
Patrick Olekas <olekaspt@mail.uc.edu>
2022-09-09 10:24:56 -0400

    Initial

---
+++ StackQueue.cpp
@@ -0,0 +1,33 @@
+// StackQueues.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "StackQueue.h"
+
+
+int ArrayBasedStack::peek() const
+{
+	if (this->isEmpty())
+	{
+		throw "ADT is emtpy";
+	}
+	else
+	{
+		//TODO
+	}
+
+
+}
+
+std::string ArrayBasedQueue::peekFront() const
+{
+	if (this->isEmpty())
+	{
+		throw "ADT is emtpy";
+	}
+	else
+	{
+		//TODO
+	}
+
+
+}
